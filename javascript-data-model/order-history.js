d62f3b3b4562f5d161c246ad5cef64e0f500d79b
Addy Abid <addy.abeed@gmail.com>
2021-08-24 18:02:58 -0700

    in progress

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,51 +2,72 @@ var orderHistory = [
   {
     delivered: 'Aug 8, 2020',
     orderPlaced: 'Aug 4, 2020',
-    itemName: 'JavaScript for impatient programmers',
-    author: 'Dr. Alex Rauschmayer',
     orderNumber: '114-3941689-8772232',
     shipTo: 'JS Masher',
-    itemCost: '$31.55',
-    total: '$34.00',
-    returnBy: 'Sep 7, 2020'
+    total: 34.00,
+    returnBy: 'Sep 7, 2020',
+    item: [
+      {
+        type: 'book',
+        itemName: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemCost: 31.55
+      }
+    ]
   },
+
   {
     delivered: 'Jul 20, 2020',
     orderPlaced: 'Jul 19, 2020',
-    itemName: 'The Timeless Way of Building',
-    author: 'Christopher Alexander',
     orderNumber: '113-9984268-1280257',
     shipTo: 'JS Masher',
-    itemCost: '$41.33',
-    total: '$44.53',
-    returnBy: 'Aug 19, 2020'
+    total: 44.53,
+    returnBy: 'Aug 19, 2020',
+    item: [
+      {
+        type: 'book',
+        itemName: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemCost: 41.33
+      }
+    ]
   },
+
   {
     delivered: 'Jul 7, 2020',
     orderPlaced: 'Jul 4, 2020',
-    itemName: 'Gamecube Controller Adapter',
     orderNumber: '114-2875557-9059409',
     shipTo: 'JS Masher',
-    itemCost: '$15.98',
-    total: '$17.22',
-    returnBy: 'Aug 5, 2020'
+    total: 17.22,
+    returnBy: 'Aug 5, 2020',
+    item: [
+      {
+        type: 'hardware',
+        itemName: 'Gamecube Controller',
+        itemCost: 15.98
+      }
+    ]
   },
+
   {
     delivered: 'Jul 5, 2020',
     orderPlaced: 'Jul 3, 2020',
     orderNumber: '113-2883177-2648248',
     shipTo: 'JS Masher',
-    total: '$138.93',
+    total: 138.93,
     returnBy: 'Aug 4, 2020',
-    items: [
+    item: [
       {
+        type: 'hardware',
         itemName: 'Gamecube Controller',
-        itemCost: '$94.95'
+        itemCost: 94.95
       },
+
       {
+        type: 'book',
         itemName: 'The Are of Sql',
         author: 'Stephanie Faroult',
-        itemCost: '$33.99'
+        itemCost: 33.99
       }
     ]
   }
