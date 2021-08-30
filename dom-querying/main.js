aab783ef91bcceb340422e8f0b8a01bfab10bf16
Addy Abid <addy.abeed@gmail.com>
2021-08-30 11:00:08 -0700

    Dom Querying

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var head = document.querySelector('h1');
+console.log(head);
+console.dir(head);
+
+var explain = document.querySelector('p#explanation');
+console.log(explain);
+console.dir(explain);
+
+var hinting = document.querySelector('p.hint');
+console.log(hinting);
+console.dir(hinting);
+
+var allPar = document.querySelectorAll('p');
+console.log(allPar);
+
+var allEl = document.querySelectorAll('.example-link');
+console.log(allEl);
