a910ce32189b6ee9bfbca82c7c76e734d325dcd5
Addy Abid <addy.abeed@gmail.com>
2021-08-30 18:58:43 -0700

    Dom Manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -3,10 +3,22 @@ var clicks = 0;
 var $clickCount = document.querySelector('.click-count');
 
 function getClicks(event) {
-
+  clicks++;
+  $clickCount.textContent = ('Clicks: ') + clicks;
+  if (clicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (clicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
 }
+
 var $hotButton = document.querySelector('.hot-button');
 $hotButton.addEventListener('click', getClicks);
-
-clicks++;
-$clickCount.textContent = clicks;
