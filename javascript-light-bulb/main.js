66a414b624351166c69eb095ec894b3a4a9249af
Addy Abid <addy.abeed@gmail.com>
2021-08-31 15:05:23 -0700

    Javascript-Light-Bulb

---
+++ javascript-light-bulb/main.js
@@ -0,0 +1,19 @@
+var isOn = false;
+
+function circle(event) {
+
+  if (isOn) {
+    $button.className = 'off';
+    $body.className = 'body-dark';
+
+  } else {
+    isOn = true;
+    $button.className = 'body-light on';
+    $body.className = 'body-light';
+  }
+}
+
+var $button = document.querySelector('button');
+$button.addEventListener('click', circle);
+
+var $body = document.querySelector('body');
