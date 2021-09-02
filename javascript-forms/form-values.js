9ddf251d72de960a8b4c382c75c4bb5bce090f96
Addy Abid <addy.abeed@gmail.com>
2021-09-01 18:02:29 -0700

    Javascript Forms

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,18 +1,16 @@
-// var $contact = document.querySelector('#contact-form');
-// $contact.addEventListener('submit', submit);
+var $contact = document.querySelector('#contact-form');
+$contact.addEventListener('submit', submit);
 
-// function submit(event) {
-// event.preventDefault();
-// var name = $contact.elements.name.value;
-// var email = $contact.elements.email.value;
-// var message = $contact.elements.message.value;
-// var formInfo = {
-// name: name,
-// email: email,
-// message: message
-// };
-
-// }
-
-// console.log('form-info:', formInfo);
-// $contact.reset();
+function submit(event) {
+  event.preventDefault();
+  var name = $contact.elements.name.value;
+  var email = $contact.elements.email.value;
+  var message = $contact.elements.message.value;
+  var formInfo = {
+    name: name,
+    email: email,
+    message: message
+  };
+  console.log('form-info:', formInfo);
+  $contact.reset();
+}
