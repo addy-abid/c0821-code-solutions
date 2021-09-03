0e6b8468ccbe29eeb5aaaf3c380f69560837f976
Addy Abid <addy.abeed@gmail.com>
2021-09-03 13:30:29 -0700

    in progress

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -8,9 +8,7 @@ else, return false
 */
 
 function isVowel(char) {
-  if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
-    return true;
-  } else if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+  if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U' || char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
     return true;
   } else {
     return false;
