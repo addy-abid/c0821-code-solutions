d9644e9d941773a5ac155f7e96b2c1715c239920
Addy Abid <addy.abeed@gmail.com>
2021-09-15 10:58:47 -0700

    Javascript Timers

---
+++ javascript-timers/interval.js
@@ -0,0 +1,14 @@
+var $h1 = document.querySelector('h1');
+$h1.textContent = '4';
+function countDown() {
+  if ($h1.textContent === '4') {
+    $h1.textContent = '3';
+  } else if ($h1.textContent === '3') {
+    $h1.textContent = '2';
+  } else if ($h1.textContent === '2') {
+    $h1.textContent = '1';
+  } else if ($h1.textContent === '1') {
+    $h1.textContent = '~Earth Beeeelooowww Us~';
+  }
+}
+setInterval(countDown, 2000);
