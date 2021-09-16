839fafc901fb2798a6fbbb6c0090c6894a15ded2
Addy Abid <addy.abeed@gmail.com>
2021-09-15 20:24:21 -0700

    Javascript Bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,42 @@
 /* exported Account */
+
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount <= 0 || !Number.isInteger(amount)) {
+    return false;
+  }
+  var deposit = new Transaction('deposit', amount);
+  this.transactions.push(deposit);
+  return true;
+
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount <= 0 || !Number.isInteger(amount)) {
+    return false;
+  }
+  var withdraw = new Transaction('withdrawal', amount);
+  this.transactions.push(withdraw);
+  return true;
+};
+
+Account.prototype.getBalance = function () {
+  var depositTotal = 0;
+  var withdrawTotal = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      depositTotal += this.transactions[i].amount;
+    }
+    if (this.transactions[i].type === 'withdrawal') {
+      withdrawTotal += this.transactions[i].amount;
+    }
+  }
+  var total = depositTotal - withdrawTotal;
+  return total;
+
+};
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,35 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance <= 0 || !Number.isInteger(balance)) {
+    return null;
+  }
+  var openAccount = new Account(this.nextAccountNumber, holder);
+
+  openAccount.transactions.push({ type: 'deposit', amount: balance });
+  openAccount.number = this.nextAccountNumber;
+  this.accounts.push(openAccount);
+  this.nextAccountNumber++;
+  return openAccount.number;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var total = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    total += this.accounts[i].getBalance();
+  }
+  return total;
+};
